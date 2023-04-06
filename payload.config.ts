45e7bd2bb2134d26288a563d5e15f5f47374bce7
Elliot DeNolf <denolfe@gmail.com>
2023-04-06 10:13:21 -0400

    feat: use payloadCloud plugin

--- payload.config.ts
+++ payload.config.ts
@@ -1,14 +1,17 @@
 import { buildConfig } from 'payload/config';
 import dotenv from 'dotenv';
+import { payloadCloud } from '@payloadcms/plugin-cloud';
 import Page from './collections/Page';
 import Media from './collections/Media';
 
 dotenv.config();
 
 export default buildConfig({
-  serverURL: process.env.PAYLOAD_PUBLIC_SERVER_URL,
   collections: [
     Page,
     Media,
+  ],
+  plugins: [
+    payloadCloud(),
   ],
 });
